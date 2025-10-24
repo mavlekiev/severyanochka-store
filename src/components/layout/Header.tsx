19aa5af303eb63e1c193a0bdb4acfef358dfc1e5
mavlekiev <mavlekiev@gmail.com>
2025-10-24 19:12:28 +0300

    refactor: refactor header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,12 +9,14 @@ import HeartIcon from "../ui/HeartIcon";
 import BurgerIcon from "../ui/BurgerIcon";
 import Logo from "../ui/Logo";
 
+const styleForLink = "flex flex-col items-center gap-2 text-black-100 font-medium text-xs leading-[1.5] transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer";
+
 export default function Header() {
   return (
     <header className="bg-white-100  sticky top-0 z-50 shadow-custom-shadow">
-      <div className="container max-w-[1248px] mx-auto px-4 py-2 sm:px-5 sm:py-[11px]">
-        <div className="sm:flex items-center justify-between md:pr-6">
-          <div className="flex items-center justify-between gap-5 md:gap-10">
+      <div className="container max-w-[1248px] mx-auto px-4 py-2 sm:px-5 sm:py-[11px] lg:pr-[44px]">
+        <div className="sm:flex items-center justify-between">
+          <div className="flex items-center justify-between max-lg:gap-5 gap-10">
             <Link
               className="transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer"
               href="/"
@@ -30,25 +32,25 @@ export default function Header() {
             </Link>
           </div>
 
-          <div className="hidden sm:flex items-center gap-[17px] md:gap-10">
-            <nav className="flex items-center gap-4 md:gap-6">
+          <div className="hidden sm:flex items-center max-lg:gap-4 lg:gap-[35px]">
+            <nav className="flex items-center max-lg:gap-4 lg:gap-6">
               <Link
                 href="/favorites"
-                className="flex flex-col items-center gap-2 text-black-100 font-medium text-xs leading-[1.5] transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer"
+                className={styleForLink}
               >
                 <HeartIcon />
                 Избранное
               </Link>
               <Link
                 href="/orders"
-                className="flex flex-col items-center gap-2 text-black-100 font-medium text-xs leading-[1.5] transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer"
+                className={styleForLink}
               >
                 <BoxIcon />
                 Заказы
               </Link>
               <Link
                 href="/cart"
-                className="flex flex-col items-center gap-2 text-black-100 font-medium text-xs leading-[1.5] relative transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer"
+                className={styleForLink}
               >
                 <CartIcon />
                 <span className="absolute -top-1 -right-[-16] bg-orange-100 text-white-100 text-[10px] rounded-[40%] w-3 h-3 flex items-center justify-center">
@@ -57,8 +59,8 @@ export default function Header() {
                 Корзина
               </Link>
             </nav>
-            <div className="hidden sm:flex items-center gap-8 transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer">
-              <div className="flex items-center gap-[10px]  p-1 md:p-0">
+            <div className="hidden sm:flex items-center gap-[26px] transition-all duration-300 ease-in-out hover:text-orange-100 group cursor-pointer">
+              <div className="flex items-center gap-[10px]  p-[5px]">
                 <Image
                   className="rounded-full"
                   src="/avatar.jpg"
@@ -67,11 +69,11 @@ export default function Header() {
                   height={40}
                   priority
                 />
-                <span className="hidden md:block text-base font-medium leading-normal">
+                <span className="max-lg:hidden text-base font-medium leading-normal">
                   Алексей
                 </span>
               </div>
-              <div className="hidden md:block">
+              <div className="max-lg:hidden">
                 <ArrowDownIcon />
               </div>
             </div>
